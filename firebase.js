903600ac5adb01985ac996ee16ba5af2a3a96195
YamGiftET <yamgift1221@gmail.com>
2026-09-02 17:15:48 +0300

    Use Firebase credentials from environment

--- firebase.js
+++ firebase.js
@@ -1,24 +1,30 @@
 const { initializeApp, getApps, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 const path = require("path");
+const fs = require("fs");
 
-const serviceAccountPath = path.join(
+let serviceAccount;
+
+if (process.env.YAM_FIREBASE_SERVICE_ACCOUNT_JSON) {
+  serviceAccount = JSON.parse(process.env.YAM_FIREBASE_SERVICE_ACCOUNT_JSON);
+} else {
+  const serviceAccountPath = path.join(
     __dirname,
     "firebase-service-account.json"
-);
-
-const serviceAccount = require(serviceAccountPath);
+  );
+  serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
+}
 
 const firebaseApp =
-    getApps().length === 0
-        ? initializeApp({
-            credential: cert(serviceAccount)
-        })
-        : getApps()[0];
+  getApps().length === 0
+    ? initializeApp({
+        credential: cert(serviceAccount)
+      })
+    : getApps()[0];
 
 const db = getFirestore(firebaseApp);
 
 module.exports = {
-    firebaseApp,
-    db
+  firebaseApp,
+  db
 };
